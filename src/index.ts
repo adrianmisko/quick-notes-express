7c7a4b34afaba234f451e5f5959694a525da58b0
adrian <adrian.misko5@gmail.com>
2019-06-03 11:07:27 +0200

    Rewrite index.js to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-const express = require( "express" );
+import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+app.get( "/", (req: any, res: any) => {
     res.send( "Hello world!" );
-} );
+});
 
 // start the Express server
-app.listen( port, () => {
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
