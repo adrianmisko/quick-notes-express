e6dfed7a56f188d82e6c6fee14006c4967fe95e8
adrian <adrian.misko5@gmail.com>
2019-06-03 11:14:14 +0200

    Configure Express to use EJS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 const port = 8080; // default port to listen
 
@@ -6,6 +8,10 @@ const port = 8080; // default port to listen
 app.get( "/", (req: any, res: any) => {
     res.send( "Hello world!" );
 });
+
+
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
 
 // start the Express server
 app.listen(port, () => {
