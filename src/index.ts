fe854b698615c40da61861d47b5c3aa641adaf51
adrian <adrian.misko5@gmail.com>
2019-06-03 17:15:08 +0200

    Refactor index

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,13 @@ const port = process.env.SERVER_PORT;
 
 const app = express();
 
-
 // define a route handler for the default home page
-app.get( "/", (req: any, res: any) => {
-    res.render( "index" );
+app.get("/", (req: any, res: any) => {
+    res.render("index");
 });
 
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // start the Express server
 app.listen(port, () => {
