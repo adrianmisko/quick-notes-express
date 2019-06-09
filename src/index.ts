4de38d37e2e0c3275b3f9a077d543e28ee48c82a
adrian <adrian.misko5@gmail.com>
2019-06-09 22:44:14 +0200

    Refactor index to use createServer in order to intialize database properly

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const port = process.env.PORT || 8080;
 
 (async () => {
-    await sequelize.sync({ force: true });
-    await createMockData();
+    await sequelize.sync();
+    // await createMockData();
     createServer(app).listen(port, () => console.info(`Server running on port ${port}`));
 })();
